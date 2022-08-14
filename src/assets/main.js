122c88c49ddad7f8e3425c32350c7b17d0688acd
suchmolin <suchmolin11@gmail.com>
2022-08-13 22:45:41 -0500

    arreglando la cantidad de videos y ordenando

--- src/assets/main.js
+++ src/assets/main.js
@@ -34,7 +34,7 @@ async function fetchData (urlApi){
                     </div>
                 </div>
             
-            `)}
+            `).slice(0,8).join('')}
         `;
         content.innerHTML = view;
         
